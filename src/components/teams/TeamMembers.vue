391affab2764624f6ba98fcd6ac027e1efbc555c
Yoon <kimyam1008@gmail.com>
2024-12-11 21:46:18 +0900

    중첩 route

--- src/components/teams/TeamMembers.vue
+++ src/components/teams/TeamMembers.vue
@@ -18,6 +18,7 @@ import UserItem from '../users/UserItem.vue';
 
 export default {
   inject: ['users', 'teams'],
+  props: ['teamId'],
   components: {
     UserItem
   },
@@ -28,8 +29,7 @@ export default {
     };
   },
   methods: {
-    loadTeamMembers(route){
-      const teamId = route.params.teamId;
+    loadTeamMembers(teamId){
       const selectedTeam = this.teams.find(team => team.id === teamId);
       const members = selectedTeam.members;
       const selectedMembers = [];
@@ -43,7 +43,7 @@ export default {
   },
   created(){
     //this.$route.path // /teams/t1
-    this.loadTeamMembers(this.$route);
+    this.loadTeamMembers(this.teamId);
    
   },
   watch: {
--- src/main.js
+++ src/main.js
@@ -5,14 +5,24 @@ import App from './App.vue';
 import TeamsList from './components/teams/TeamsList.vue';
 import UsersList from './components/users/UsersList.vue';
 import TeamMembers from './components/teams/TeamMembers.vue';
+import NotFound from './components/nav/NotFound.vue';
 
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
-        { path: '/teams', component: TeamsList }, // our-domain.com/teams => TeamsList!
+        { path: '/', redirect: '/teams'}, //redirect: '/teams' or TeamsList 컴포넌트 부분에 alias: '/' 추가  
+        { 
+          name: 'teams',
+          path: '/teams',
+          component: TeamsList,
+          children: [
+            {name: 'team-members', path: ':teamId', component: TeamMembers, props: true}, // props: true 프로퍼티로서 컴포넌트에 전달되어야 한다고 알려주는 기능
+          ] // name 사용이유: 가독성 및 유지보수 쉬움
+        }, // our-domain.com/teams => TeamsList! 
         { path: '/users', component: UsersList },
-        { path: '/teams/:teamId', component: TeamMembers},
+        
+        { path: '/:notFound(.*)', component: NotFound} //순서 주의
     ],
     linkActiveClass: 'active'
 });
